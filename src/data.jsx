f65730661b855ba45f1a26fd73f7cbeb2ee0a6f3
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-24 16:17:22 -0300

    chore: Move card images to public dir

--- src/data.jsx
+++ src/data.jsx
@@ -5,7 +5,7 @@ export default [
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
-    coverImg: "../src/assets/KatieZaferes.png",
+    coverImg: "./KatieZaferes.png",
     imgAlt: "A picture of Katie Zaferes smilling",
     stats: {
       rating: 5.0,
@@ -20,7 +20,7 @@ export default [
     description:
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
-    coverImg: "../src/assets/WeddingPhotography.png",
+    coverImg: "./WeddingPhotography.png",
     imgAlt: "A picture of a woman wearing a wedding dress",
     stats: {
       rating: 5.0,
@@ -35,7 +35,7 @@ export default [
     description:
       "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 50,
-    coverImg: "../src/assets/MountainBike.png",
+    coverImg: "./MountainBike.png",
     imgAlt: "A picture of a bike on top of a mountain",
     stats: {
       rating: 4.0,
