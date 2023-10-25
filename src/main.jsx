714ea31fbf746a779c8c8d9ffb073bb64336f656
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-10-25 12:20:11 -0300

    refactor: Consolidate font styles into index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
-import "./styles/fonts.css";
 import "./styles/header.css";
 import "./styles/hero.css";
 import "./styles/card.css";
